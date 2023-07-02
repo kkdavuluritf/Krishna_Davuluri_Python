afbfb80f28f1f76c7e50da72b809b9ba69529483
kkdavuluritf <kkdavuluri@gmail.com>
2023-07-01 19:29:18 -0500

    For Python Lab

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ## to do
 print("Enter a todo:")
-##string1 = input("Enter a todo:")
+#string1 = input("Enter atodo:")
 #str1 = " Please"
 str1 = int(input("Please give an input:\t\t"))
 print (str1)
