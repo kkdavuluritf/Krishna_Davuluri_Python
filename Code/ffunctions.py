81807eadf360d320b8edc966c2301e408bccf83d
umasree davuluri <umasreedavuluri9@gmail.com>
2023-07-10 22:12:17 -0500

    For July10th

---
+++ Code/ffunctions.py
@@ -0,0 +1,7 @@
+number1 = float(input("Enter number 1:\t\t"))
+#number2 = float(input("Enter number 2:\t\t"))
+
+#number3 = number1+str({25*4+100})+number2
+#print(f"Value is\t {number1} {25*4+100} {number2}")
+number2 = number1*365.25
+print(f"Age in days\t{number1*365.25} \nCalculated days\t{number2}")
